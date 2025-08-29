f28b140892f5d83415fc964767cb4602a9a12b7c
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-08-28 21:22:08 -0500

    Actualizacion de rutas de enlace del footer

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('a_EncabezadoFooter.princi');
 })->name('home');
+//---------------------------------- CONTENIDO DE FOOTER + INICIO ---------------------------------------------------//
+// codigo essna
+Route::get('/esna', function () {
+    return view('h_footer.esna');
+})->name('esna');
+//codigo pagos
+Route::get('/pagos', function () {
+    return view('h_footer.pagos');
+})->name('pagos');
+//politicas de privacidad
+Route::get('/privaty', function () {
+    return view('h_footer.privaty');
+})->name('privaty');
+//terminos y condiciones
+Route::get('/terminos', function () {
+    return view('h_footer.terminos');
+})->name('terminos');
+//---------------------------------- CONTENIDO DE FOOTER + FIN    ---------------------------------------------------//
 // Página principal
 Route::get('/', function () {
     return view('a_EncabezadoFooter.inicio');
@@ -16,25 +34,37 @@ Route::get('/nosotros', function () {
     return view('b_nosotros.nosotros');
 })->name('nosotros');
 
+// Página de servicio
+Route::get('/servicios', function () {
+    return view('d_Servicio.servicio');
+})->name('servicio');
+
 // Página de flota aérea
 Route::get('/flota', function () {
     return view('c_FlotaAerea.flota');
 })->name('flota');
+//---------------------------------- CONTENIDO DE FLOTA + INICIO ---------------------------------------------------//
 
-// Página de servicios
-Route::get('/servicios', function () {
-    return view('d_Servicio.servicio');
-})->name('servicios');
+
+//---------------------------------- CONTENIDO DE FLOTA + FIN    ---------------------------------------------------//
 
 // Página de agencia
 Route::get('/agencia', function () {
     return view('e_Agencia.agencia');
 })->name('agencia');
+//---------------------------------- CONTENIDO DE AGENCIA + INICIO ---------------------------------------------------//
+
+
+//---------------------------------- CONTENIDO DE AGENCIA + FIN    ---------------------------------------------------//
 
 // Página de blog
 Route::get('/blog', function () {
     return view('f_Blog.blog');
 })->name('blog');
+//---------------------------------- CONTENIDO DE BLOG + INICIO ---------------------------------------------------//
+
+
+//---------------------------------- CONTENIDO DE BLOG + FIN    ---------------------------------------------------//
 
 // Página de contacto
 Route::get('/contacto', function () {
