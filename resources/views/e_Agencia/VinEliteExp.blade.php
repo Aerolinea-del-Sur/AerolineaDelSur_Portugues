77dc1167e2f0828dcb4121c5b895a390aa2a0b06
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-08-30 22:54:14 -0500

    Actualizacion de tour vinacunca Elite experience v2

--- resources/views/e_Agencia/VinEliteExp.blade.php
+++ resources/views/e_Agencia/VinEliteExp.blade.php
@@ -1,6 +1,7 @@
 @extends('a_EncabezadoFooter.princi')
 @section('content')
 <link rel="stylesheet" href="{{ asset('public/css/VPMachuPichu.css') }}">
+<link rel="stylesheet" href="{{ asset('public/css/VinEliteExp.css') }}">
 
 <div class="tour-container">
     <!-- Hero Section -->
