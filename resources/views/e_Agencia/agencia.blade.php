e5b78de9abf00039590f788b416ab016715ec9b0
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-09-09 23:44:28 -0500

    Actualizacion de error de imagen

--- resources/views/e_Agencia/agencia.blade.php
+++ resources/views/e_Agencia/agencia.blade.php
@@ -67,7 +67,7 @@
                 <!-- Tour 01: Andes Mágicos -->
                 <div class="tour-card" data-category="sobrevuelo">
                     <div class="tour-image">
-                        <img src="https://player.vimeo.com/video/1117325379?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&loop=1&muted=1&background=1&controls=0" alt="Líneas de Nazca">
+                        <img src="https://images.unsplash.com/photo-1587595431973-160d0d94add1?ixlib=rb-4.0.3&auto=format&fit=crop&w=1920&q=80" alt="Líneas de Nazca">
                         <div class="tour-overlay">
                             <div class="tour-badge sobrevuelo">Sobrevuelo</div>
                             <!-- <div class="tour-price">Desde $199</div>-->
