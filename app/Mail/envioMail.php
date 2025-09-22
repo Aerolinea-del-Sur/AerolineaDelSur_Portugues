9ddf0825daf176fbdb5b3003ec4a8a871c51954c
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-09-21 20:14:30 -0500

    Actualizacion de controller

--- app/Mail/envioMail.php
+++ app/Mail/envioMail.php
@@ -6,14 +6,54 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-/*    
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
-*/
 
 class envioMail extends Mailable
 {
     use Queueable, SerializesModels;
+    
+    /**
+     * message instance.
+     *
+     * @return \Illuminate\Mail\Mailables\Attachment>
+     */
+
+    public function _construct()
+    {
+        //
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'test laravel 12',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'g_contactos.prueba',
+        );
+    }
+
+    /**
+     * message.
+     *
+     * @return \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+    /*public function build()
+    {
+        return $this->from(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'))
+                    ->view('testmail')
+                    ->subject('Prueba de Correo')
+                    ->with($this->data);
+    }*/
     /* 
     public $data;
     
@@ -37,27 +77,4 @@ class envioMail extends Mailable
         );
     }
     */
-    /**
-     * message instance.
-     *
-     * @return \Illuminate\Mail\Mailables\Attachment>
-     */
-    public $data;
-    public function _construct($data)
-    {
-        $this->data = $data;
-    }
-    /**
-     * message.
-     *
-     * @return \Illuminate\Mail\Mailables\Attachment>
-     */
-
-    public function build()
-    {
-        return $this->from(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'))
-                    ->view('testmail')
-                    ->subject('Prueba de Correo')
-                    ->with($this->data);
-    }
 }
