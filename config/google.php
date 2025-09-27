840f4668dc2c4cfa6f019e00ebd03deeafab779e
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-09-26 23:59:34 -0500

    Fase de prueba de api v2

--- config/google.php
+++ config/google.php
@@ -1,8 +1,14 @@
 <?php
 return [
-    'client_id' => env('GOOGLE_CLIENT_ID'),
-    'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-    'redirect_uri' => env('GOOGLE_REDIRECT_URI'),
-    'refresh_token' => env('GOOGLE_REFRESH_TOKEN'),
+    'project' => [
+        'name' => 'GmailAPI',
+        'number' => '611641103456',
+        'id' => 'gmailapi-472906'
+    ],
+    'oauth' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect_uri' => env('GOOGLE_REDIRECT_URI'),
+        'refresh_token' => env('GOOGLE_REFRESH_TOKEN')
+    ]
 ];
-?>
