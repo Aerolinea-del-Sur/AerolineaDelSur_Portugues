2b70714697d339e3ae9ad12b2c25dfcbe23f790a
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-09-27 00:16:50 -0500

    Fase de prueba de api v4

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,53 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\Email\Email;
-use App\Services\Email\PHPMailerMessage;
-use App\Services\Email\SwiftMailerMessage;
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
-class EmailController extends Controller
+class Controller extends BaseController
 {
-    public function enviarEmail(Request $request)
-    {
-        try {
-            $email = new Email();
-            
-            $resultado = $email->setTo($request->email)
-                              ->setSubject($request->asunto)
-                              ->setBody($request->mensaje)
-                              ->send();
-
-            return response()->json(['mensaje' => 'Correo enviado exitosamente']);
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
-        }
-    }
-
-    public function enviarPHPMailer()
-    {
-        try {
-            $mailer = new PHPMailerMessage();
-            $mensaje = $mailer->create();
-            
-            return response()->json(['mensaje' => 'Mensaje creado exitosamente']);
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
-        }
-    }
-
-    public function enviarSwift(Request $request)
-    {
-        try {
-            $swift = new SwiftMailerMessage();
-            $swift->to($request->email)
-                  ->subject($request->asunto)
-                  ->body($request->mensaje)
-                  ->send();
-
-            return response()->json(['mensaje' => 'Correo enviado exitosamente']);
-        } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
-        }
-    }
+    use AuthorizesRequests, ValidatesRequests;
 }
