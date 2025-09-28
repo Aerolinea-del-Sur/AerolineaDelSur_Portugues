272ad2297ab19c185a4e57de6dcecddb57161b26
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-09-28 00:10:17 -0500

    Fase de prueba de token fresh v2

--- app/Http/Controllers/GmailAuthController.php
+++ app/Http/Controllers/GmailAuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Google_Client;
 use Illuminate\Support\Facades\Storage;
 
@@ -15,12 +14,10 @@ class GmailAuthController extends Controller
         $client->setAuthConfig(storage_path('app/credentials.json'));
         $client->setRedirectUri(route('gmail.callback'));
         $client->addScope('https://www.googleapis.com/auth/gmail.modify');
-        $client->setAccessType('offline'); // <- Necesario para refresh_token
-        $client->setPrompt('consent'); // <- Obligatorio para forzar el refresh_token
+        $client->setAccessType('offline');
+        $client->setPrompt('consent');
 
-        $authUrl = $client->createAuthUrl();
-
-        return redirect($authUrl);
+        return redirect($client->createAuthUrl());
     }
 
     public function handleGoogleCallback()
@@ -30,19 +27,13 @@ class GmailAuthController extends Controller
         $client->setRedirectUri(route('gmail.callback'));
 
         $code = request()->get('code');
-
         $token = $client->fetchAccessTokenWithAuthCode($code);
 
-        if (isset($token['error'])) {
-            return response()->json(['error' => $token['error']]);
-        }
-
-        // 🔥 Aquí tienes el refresh_token y access_token
         Storage::disk('local')->put('token.json', json_encode($token));
 
         return response()->json([
             'message' => 'Autenticación exitosa',
-            'token' => $token, // Puedes ver el refresh_token aquí
+            'token' => $token
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -267,5 +267,8 @@ Route::post('/email/swift', [EmailController::class, 'enviarSwift']);
 Route::get('/test-gmail', [App\Http\Controllers\ContactController::class, 'testGmailAPI']);
 Route::get('/prueba-mensaje', [App\Http\Controllers\ContactController::class, 'enviarMensajePrueba']);
 
+use App\Http\Controllers\GmailAuthController;
+
 Route::get('/auth/google', [GmailAuthController::class, 'redirectToGoogle'])->name('gmail.auth');
 Route::get('/callback', [GmailAuthController::class, 'handleGoogleCallback'])->name('gmail.callback');
+
