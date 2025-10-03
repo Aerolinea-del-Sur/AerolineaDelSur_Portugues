e61548da409f40e0ee61aa01f601fa5b35073470
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-10-02 23:16:32 -0500

    Actualziacion de logo a ruta de la pagina

--- resources/views/a_EncabezadoFooter/princi.blade.php
+++ resources/views/a_EncabezadoFooter/princi.blade.php
@@ -76,7 +76,7 @@
             <header class="header">
                 <div class="header-container">
                     <!-- Logo -->
-                    <a href="/micosina.online/" class="logo"><img src="public/img/logo.svg" alt="Aerolineas del Sur" class="logo-img"></a>
+                    <a href="https://micosina.online/" class="logo"><img src="public/img/logo.svg" alt="Aerolineas del Sur" class="logo-img"></a>
                     <!-- Pill Navigation -->
                     <nav class="nav">
                         
