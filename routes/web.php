950eab11bdae5f3af31e796af0d81f7d61007c63
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-11-12 15:22:59 -0500

    Actualizacion del mapa jerarquico del sitio

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Mail\envioMail;
+//use App\Mail\envioMail;
 
-// Página principal
+//----------------------------------      PAGINA PRINCIPAL      ---------------------------------------------------//
+
 Route::get('/', function () {
     return view('a_EncabezadoFooter.princi');
 })->name('home');
-//---------------------------------- CONTENIDO DE FOOTER + INICIO ---------------------------------------------------//
-// codigo essna
-Route::get('/esna', function () {
-    return view('h_footer.esna');
-})->name('esna');
-//codigo pagos
-Route::get('/pagos', function () {
-    return view('h_footer.pagos');
-})->name('pagos');
-//politicas de privacidad
-Route::get('/privaty', function () {
-    return view('h_footer.privaty');
-})->name('privaty');
-//terminos y condiciones
-Route::get('/terminos', function () {
-    return view('h_footer.terminos');
-})->name('terminos');
-//Libro de reclamaciones
-Route::get('/libro', function () {
-    return view('h_footer.libro'); 
-})->name('libro');
-//---------------------------------- CONTENIDO DE FOOTER + FIN    ---------------------------------------------------//
-// Página principal
+
 Route::get('/', function () {
     return view('a_EncabezadoFooter.inicio');
 })->name('inicio');
 
+//----------------------------------    HEADER    ---------------------------------------------------//
 // Página de nosotros
 Route::get('/nosotros', function () {
     return view('b_nosotros.nosotros');
@@ -48,116 +28,148 @@ Route::get('/servicio', function () {
 Route::get('/aeronaves', function () {
     return view('c_Aeronaves.aeronaves');
 })->name('aeronaves');
-//---------------------------------- CONTENIDO DE FLOTA + INICIO ---------------------------------------------------//
+
+// Página de contacto
+Route::get('/contacto', function () {
+    return view('g_contactos.contacto');
+})->name('contacto');
+
+//----------------------------------    FOOTER    ---------------------------------------------------//
+//------------------    institucional    ------------------//
+
+// codigo essna
+Route::get('/institucional/esna', function () {
+    return view('h_footer.esna');
+})->name('esna');
+//codigo pagos
+Route::get('/institucional/pagos', function () {
+    return view('h_footer.pagos');
+})->name('pagos');
+
+//------------------    legal    ------------------//
+//politicas de privacidad
+Route::get('/legal/politicas-privacidad', function () {
+    return view('h_footer.privaty');
+})->name('privaty');
+//terminos y condiciones
+Route::get('/legal/terminos-condiciones', function () {
+    return view('h_footer.terminos');
+})->name('terminos');
+//Libro de reclamaciones
+Route::get('/legal/libro-reclamaciones', function () {
+    return view('h_footer.libro'); 
+})->name('libro');
+
+//----------------------------------    AERONAVES    ---------------------------------------------------//
 
 // Página del Avión Gulfstream G100
-Route::get('/GulfstreamG100', function () {
+Route::get('/aeronaves/GulfstreamG100', function () {
     return view('c_Aeronaves.GulfstreamG100');
 })->name('GulfstreamG100');
 
 // Página del Avión Gulfstream G IV
-Route::get('/GulfstreamGIV', function () {
+Route::get('/aeronaves/GulfstreamGIV', function () {
     return view('c_Aeronaves.GulfstreamGIV'); 
 })->name('GulfstreamGIV');
 
 // Página del Honda Jet
-Route::get('/HondaJet', function () {
+Route::get('/aeronaves/HondaJet', function () {
     return view('c_Aeronaves.HondaJet'); 
 })->name('HondaJet');
 
 // Página del Phenom 100
-Route::get('/Phenom100', function () {
+Route::get('/aeronaves/Phenom100', function () {
     return view('c_Aeronaves.Phenom100'); 
 })->name('Phenom100');
 
 // Página del Phenom 300
-Route::get('/Phenom300', function () {
+Route::get('/aeronaves/Phenom300', function () {
     return view('c_Aeronaves.Phenom300'); 
 })->name('Phenom300');
 
 // Página del Challenger 300
-Route::get('/Challenger300', function () {
+Route::get('/aeronaves/Challenger300', function () {
     return view('c_Aeronaves.Challenger300'); 
 })->name('Challenger300'); 
 
 // Página del Challenger 350
-Route::get('/Challenger350', function () {
+Route::get('/aeronaves/Challenger350', function () {
     return view('c_Aeronaves.Challenger350'); 
 })->name('Challenger350'); 
 
 // Página del Challenger 601
-Route::get('/Challenger601', function () {
+Route::get('/aeronaves/Challenger601', function () {
     return view('c_Aeronaves.Challenger601'); 
 })->name('Challenger601'); 
 
 // Página del Challenger 604
-Route::get('/Challenger604', function () {
+Route::get('/aeronaves/Challenger604', function () {
     return view('c_Aeronaves.Challenger604'); 
 })->name('Challenger604'); 
 
 // Página del Challenger 605
-Route::get('/Challenger605', function () {
+Route::get('/aeronaves/Challenger605', function () {
     return view('c_Aeronaves.Challenger605'); 
 })->name('Challenger605');
 
 // Página del Challenger 650
-Route::get('/Challenger650', function () {
+Route::get('/aeronaves/Challenger650', function () {
     return view('c_Aeronaves.Challenger650'); 
 })->name('Challenger650'); 
 
 // Página del Challenger 850
-Route::get('/Challenger850', function () {
+Route::get('/aeronaves/Challenger850', function () {
     return view('c_Aeronaves.Challenger850'); 
 })->name('Challenger850'); 
 
 // Página del Citation 550 Bravo
-Route::get('/Citation550Bravo', function () {
+Route::get('/aeronaves/Citation550Bravo', function () {
     return view('c_Aeronaves.Citation550Bravo'); 
 })->name('Citation550Bravo'); 
 
 // Página del Citation Cj4
-Route::get('/CitationCj4', function () {
+Route::get('/aeronaves/CitationCj4', function () {
     return view('c_Aeronaves.CitationCj4'); 
 })->name('CitationCj4'); 
 
 // Página del Citation Encore
-Route::get('/CitationEncore', function () {
+Route::get('/aeronaves/CitationEncore', function () {
     return view('c_Aeronaves.CitationEncore'); 
 })->name('CitationEncore'); 
 
 // Página del Citation XLS
-Route::get('/CitationXLS', function () {
+Route::get('/aeronaves/CitationXLS', function () {
     return view('c_Aeronaves.CitationXLS'); 
 })->name('CitationXLS'); 
 
 // Página del Citation HemisPhere
-Route::get('/CitationHemisPhere', function () {
+Route::get('/aeronaves/CitationHemisPhere', function () {
     return view('c_Aeronaves.CitationHemisPhere'); 
 })->name('CitationHemisPhere'); 
 
 // Página del Citation Latitude
-Route::get('/CitationLatitude', function () {
+Route::get('/aeronaves/CitationLatitude', function () {
     return view('c_Aeronaves.CitationLatitude'); 
 })->name('CitationLatitude'); 
 
 // Página del MI 8 MTV 1
-Route::get('/Mi8-mtv1', function () {
+Route::get('/aeronaves/Mi8-mtv1', function () {
     return view('c_Aeronaves.Mi8-mtv1'); 
 })->name('Mi8-mtv1'); 
 
 // Página del Ecureuil B3
-Route::get('/Ecureuil-b3', function () {
+Route::get('/aeronaves/Ecureuil-b3', function () {
     return view('c_Aeronaves.Ecureuil-b3'); 
 })->name('Ecureuil-b3');
 
-//---------------------------------- CONTENIDO DE FLOTA + FIN    ---------------------------------------------------//
+//---------------------------------- AGENCIA ---------------------------------------------------//
 
 // Página de agencia
 Route::get('/agencia', function () {
     return view('e_Agencia.agencia');
 })->name('agencia');
 
-//---------------------------------- CONTENIDO DE AGENCIA + INICIO - estructura semantica ---------------------------------------------------//
+//---------------------------------- TOURS Y SOBREVUELO  ---------------------------------------------------//
 // etiquetado paquetes = P, tours = T, sobre vuelos = S
 
 // Página de andes-magicos
@@ -190,35 +202,20 @@ Route::get('/agencia/tour/vinicunca', function () {
     return view('e_Agencia.tour-vinicunca');
 })->name('tour-vinicunca'); 
 
-//---------------------------------- CONTENIDO DE AGENCIA + FIN    ---------------------------------------------------//
-
+//---------------------------------- BLOG    ---------------------------------------------------//
 // Página de blog
 Route::get('/blog', function () {
     return view('f_Blog.blog');
 })->name('blog');
 
-//---------------------------------- CONTENIDO DE BLOG + INICIO ---------------------------------------------------//
+//---------------------------------- BLOG ---------------------------------------------------//
 
+//---------------------------------- CONTROLADORES + FORMULARIO + SERVICIO GOOGLE ---------------------------------------------------//
 
-//---------------------------------- CONTENIDO DE BLOG + FIN    ---------------------------------------------------//
-
-// Página de contacto
-Route::get('/contacto', function () {
-    return view('g_contactos.contacto');
-})->name('contacto');
-
-// Página de contacto
-Route::get('/tours', function () {
-    return view('tour.tours');
-})->name('tours');
-//---------------------------------- CORREO ---------------------------------------------------//
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AircraftController;
 
 Route::post('/enviar-contacto', [ContactController::class, 'send'])
 ->name('contact.send');
-
-
-use App\Http\Controllers\AircraftController;
-
 Route::post('/enviar-solicitud-aeronave', [AircraftController::class, 'sendAircraftInquiry'])
 ->name('aircraft.inquiry.send');
