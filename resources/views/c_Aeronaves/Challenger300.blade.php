6571d93f5caceb2fd24cd22c533a2525d76e1960
Jose Amilcar Aller Laime <jos22.work@gmail.com>
2025-11-14 00:19:36 -0500

    Correccion de inmagenes C300

--- resources/views/c_Aeronaves/Challenger300.blade.php
+++ resources/views/c_Aeronaves/Challenger300.blade.php
@@ -151,16 +151,16 @@
                         <div class="new-carousel-container">
                             <div class="new-carousel-track">
                                 <div class="new-carousel-slide current-slide">
-                                    <img src="public/img/aeronaves/aviones/Challenger300-1.webp" alt="" class="carousel-image">
+                                    <img src="/public/img/aeronaves/aviones/Challenger300-1.webp" alt="" class="carousel-image">
                                 </div>
                                 <div class="new-carousel-slide">
-                                    <img src="public/img/aeronaves/aviones/Challenger300-2.webp" alt="" class="carousel-image">
+                                    <img src="/public/img/aeronaves/aviones/Challenger300-2.webp" alt="" class="carousel-image">
                                 </div>
                                 <div class="new-carousel-slide">
-                                    <img src="public/img/aeronaves/aviones/Challenger300-3.webp" alt="" class="carousel-image">
+                                    <img src="/public/img/aeronaves/aviones/Challenger300-3.webp" alt="" class="carousel-image">
                                 </div>
                                 <div class="new-carousel-slide">
-                                    <img src="public/img/aeronaves/aviones/Challenger300-4.webp" alt="" class="carousel-image">
+                                    <img src="/public/img/aeronaves/aviones/Challenger300-4.webp" alt="" class="carousel-image">
                                 </div>
                             </div>
                         </div>
