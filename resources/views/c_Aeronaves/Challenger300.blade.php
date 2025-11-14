800e8297eae807151c5f3c037ed1cd29c5ee7e09
Jose Amilcar Aller Laime <jos22.work@gmail.com>
2025-11-14 01:42:10 -0500

    Correccion de inmagenes C300 prueba 4

--- resources/views/c_Aeronaves/Challenger300.blade.php
+++ resources/views/c_Aeronaves/Challenger300.blade.php
@@ -157,25 +157,21 @@ $h3_14 = 'Solicitar Información';
                         <div class="new-carousel-container">
                             <div class="new-carousel-track">
                                 <div class="new-carousel-slide current-slide">
-                                    <img src="/public/img/aeronaves/aviones/Challenger300-1.webp" alt=""
-                                        class="carousel-image">
+                                    <img src="/public/img/aeronaves/aviones/Challenger300-1.webp" alt="" class="carousel-image">
                                 </div>
                                 <div class="new-carousel-slide">
-                                    <img src="/public/img/aeronaves/aviones/Challenger300-2.webp" alt=""
-                                        class="carousel-image">
+                                    <img src="/public/img/aeronaves/aviones/Challenger300-2.webp" alt="" class="carousel-image">
                                 </div>
                                 <div class="new-carousel-slide">
-                                    <img src="/public/img/aeronaves/aviones/Challenger300-3.webp" alt=""
-                                        class="carousel-image">
+                                    <img src="/public/img/aeronaves/aviones/Challenger300-3.webp" alt="" class="carousel-image">
                                 </div>
                                 <div class="new-carousel-slide">
-                                    <img src="/public/img/aeronaves/aviones/Challenger300-4.webp" alt=""
-                                        class="carousel-image">
+                                    <img src="/public/img/aeronaves/aviones/Challenger300-4.webp" alt="" class="carousel-image">
                                 </div>
                             </div>
                         </div>
 
-                        <!-- Controles de navegación -->
+                        <!-- Controles de navegación --> 
                         <button class="new-carousel-btn new-carousel-btn--left" id="prevBtn">
                             <i class="fas fa-chevron-left"></i>
                         </button>
