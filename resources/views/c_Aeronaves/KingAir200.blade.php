ee2a406976767382f6c8ebfded93e0db5f876988
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-11-16 12:36:08 -0500

    Actualizacion de aeronave king foto v8

--- resources/views/c_Aeronaves/KingAir200.blade.php
+++ resources/views/c_Aeronaves/KingAir200.blade.php
@@ -66,7 +66,7 @@
     <script>
         // Configurar clase body para imagen de portada específica
         document.addEventListener('DOMContentLoaded', function() {
-            document.body.classList.add('Air-King-B200');
+            document.body.classList.add('air-king-b200');
         });
     </script>
 <!-- Encabezado Tradicional Elegante -->
@@ -153,7 +153,7 @@
                             <div class="new-carousel-track">
                                 <div class="new-carousel-slide current-slide">
                                     <img
-                                        src="{{ asset('img/aeronaves/aviones/Air-King-B200-1.webp') }}"
+                                        src="/public/img/aeronaves/aviones/Air-King-B200-1.webp"
                                         alt="king air"
                                         class="carousel-image"
                                         sizes="(max-width: 890px) 550px, 
@@ -182,7 +182,7 @@
                                 </div>
                                 <div class="new-carousel-slide">
                                     <img
-                                        src="{{ asset('img/aeronaves/aviones/Air-King-B200-2.webp') }}"
+                                        src="/public/img/aeronaves/aviones/Air-King-B200-2.webp"
                                         alt="king air 200"
                                         class="carousel-image"
                                         sizes="(max-width: 890px) 550px, 
@@ -211,7 +211,7 @@
                                 </div>
                                 <div class="new-carousel-slide">
                                     <img
-                                        src="{{ asset('img/aeronaves/aviones/Air-King-B200-3.webp') }}"
+                                        src="/public/img/aeronaves/aviones/Air-King-B200-3.webp"
                                         alt="king 200"
                                         class="carousel-image"
                                         sizes="(max-width: 890px) 550px, 
@@ -240,7 +240,7 @@
                                 </div>
                                 <div class="new-carousel-slide">
                                     <img
-                                        src="{{ asset('img/aeronaves/aviones/Air-King-B200-4.webp') }}"
+                                        src="/public/img/aeronaves/aviones/Air-King-B200-4.webp"
                                         alt="king air b200"
                                         class="carousel-image"
                                         sizes="(max-width: 890px) 550px, 
@@ -388,7 +388,7 @@
                             </div>
                         </div>
                         <div class="seating-diagram">
-                            <img src="{{ asset('img/aeronaves/aviones/Air-King-B200.webp') }}" alt="" style="width: 100%; height: 100%; border-radius: 10px; object-fit: cover;">
+                            <img src="public/img/aeronaves/aviones/Air-King-B200.webp" alt="" style="width: 100%; height: 100%; border-radius: 10px; object-fit: cover;">
                         </div>
                     </div>
                 </section>
