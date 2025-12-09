2136a7284cf2138cc9bc06a845872eec628a61e0
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-12-09 00:01:31 -0500

    Actualizacion de favicon.ico v4

--- resources/views/a_EncabezadoFooter/princi.blade.php
+++ resources/views/a_EncabezadoFooter/princi.blade.php
@@ -46,6 +46,11 @@
             <meta property="og:locale:alternate" content="en_US">
             <meta property="og:locale:alternate" content="fr_FR">
             <link rel="icon" type="image/x-icon" href="/favicon.ico">
+            <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('public/img/service/favicon_io/apple-touch-icon.png') }}">
+            <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('public/img/service/favicon_io/favicon-32x32.png') }}">
+            <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('public/img/service/favicon_io/favicon-16x16.png') }}">
+            <link rel="manifest" href="{{ asset('public/img/service/favicon_io/site.webmanifest') }}">
+            <meta name="theme-color" content="#000000">
             <!-- Twitter Card 
             <meta name="twitter:card" content="summary_large_image">
             <meta name="twitter:title" content="Russell Experience - Turismo Sostenible en Perú">
