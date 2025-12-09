3cec6383be72fbf8b649f706c93e9464f8066890
Victor Hugo Giraldez Taco <victor1999.work@gmail.com>
2025-12-09 01:12:39 -0500

    Actualizacion de favicon.ico v10

--- resources/views/a_EncabezadoFooter/princi.blade.php
+++ resources/views/a_EncabezadoFooter/princi.blade.php
@@ -53,6 +53,7 @@
             <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('public/favicon.ico') }}">
             <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('public/favicon.ico') }}">
             <link rel="manifest" href="{{ asset('favicon.ico') }}">
+            <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
             <meta name="theme-color" content="#000000">
         <!-- Twitter Card 
             <meta name="twitter:card" content="summary_large_image">
